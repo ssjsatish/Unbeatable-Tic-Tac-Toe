e3e377aa4d04979db397e8bcf96c57d38b0e55bc
Satish Patel <ssj.satish@gmail.com>
2016-08-07 22:31:36 +0530

    Add files via upload

--- src/main/java/LevelSelection.java
+++ src/main/java/LevelSelection.java
@@ -1,1 +1,48 @@
+package com.seafle.satish.unbeatabletictactoe;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LevelSelection extends AppCompatActivity {
+
+
+    Button normal,medium,unbeatable;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_selection);
+        normal = (Button)findViewById(R.id.normal);
+        medium = (Button)findViewById(R.id.medium);
+        unbeatable = (Button)findViewById(R.id.unbeatable);
+        normal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent normalgame = new Intent(LevelSelection.this,NormalGame.class);
+                // A simple brute force to and some what probability theory
+                Intent normalgame = new Intent(LevelSelection.this,NormalGame.class);
+                startActivity(normalgame);
+            }
+        });
+        medium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // applying normal probability theory to get this done
+                Intent mediumgame = new Intent(LevelSelection.this,MediumGame.class);
+                startActivity(mediumgame);
+
+            }
+        });
+        unbeatable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Code for implementing minimax algorithm
+                Intent unbeatable = new Intent(LevelSelection.this,UnbeatableGame.class);
+                startActivity(unbeatable);
+            }
+        });
+
+    }
+}
--- src/main/java/MainActivity.java
+++ src/main/java/MainActivity.java
@@ -1,18 +1,36 @@
 package com.seafle.satish.unbeatabletictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    Button single_player, two_player;
+    Button one_player, two_player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        single_player = (Button)findViewById(R.id.single_player);
+        one_player = (Button)findViewById(R.id.single_player);
         two_player = (Button) findViewById(R.id.two_player);
+
+        one_player.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this, LevelSelection.class);
+                startActivity(in);
+            }
+        });
+        two_player.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(MainActivity.this,TwoPlayer.class);
+                startActivity(i);
+            }
+        });
     }
 }
