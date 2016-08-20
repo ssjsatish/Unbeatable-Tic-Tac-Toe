e9a0d92a3d011eb78f99697a598d2c84cf887c93
Satish Patel <ssj.satish@gmail.com>
2016-08-21 00:09:59 +0530

    Update UnbeatableGame.java

--- src/main/java/UnbeatableGame.java
+++ src/main/java/UnbeatableGame.java
@@ -2,6 +2,7 @@ package com.seafle.satish.unbeatabletictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import java.util.*;
 import java.io.*;
@@ -190,13 +191,80 @@ public class UnbeatableGame extends AppCompatActivity {
 */
 
 
-    Button b1,b2,b3,b4,b5,b6,b7,b8,b9;
+    Button b1,b2,b3,b4,b5,b6,b7,b8,b9,switchbutton;
     int ar[][] = new int[3][3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unbeatable_game);
+        b1 = (Button)findViewById(R.id.b1);
+        b2 = (Button)findViewById(R.id.b2);
+        b3 = (Button)findViewById(R.id.b3);
+        b4 = (Button)findViewById(R.id.b4);
+        b5 = (Button)findViewById(R.id.b5);
+        b6 = (Button)findViewById(R.id.b6);
+        b7 = (Button)findViewById(R.id.b7);
+        b8 = (Button)findViewById(R.id.b8);
+        b9 = (Button)findViewById(R.id.b9);
+        switchbutton = (Button)findViewById(R.id.switchbutton);
+        
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        
+
     }
 
 
